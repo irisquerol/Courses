6aaa6d8395d664268222906e259d955f615ec84d
Iris <irisquerol1998@gmail.com>
2022-03-06 17:08:03 +0100

    Get info pantalla 2, scroll layout2, array strings

--- app/src/main/java/com/android/courses/DetailsActivity.java
+++ app/src/main/java/com/android/courses/DetailsActivity.java
@@ -10,19 +10,31 @@ import android.widget.TextView;
 public class DetailsActivity extends AppCompatActivity {
 
     private int id;
+
+    private String[] titles, descriptions, teachers, credits;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-
-        Intent intentReciver = getIntent();
-        int intValue = intentReciver.getIntExtra("subjectID", 0);
+        titles = getResources().getStringArray(R.array.titles);
+        descriptions = getResources().getStringArray(R.array.longdescription);
+        teachers = getResources().getStringArray(R.array.teachers);
+        credits = getResources().getStringArray(R.array.credits);
+        Intent intentGetter = getIntent();
+        int intValue = intentGetter.getIntExtra("subjectID", 0);
 
         setContentView(R.layout.activity_details);
-        TextView textView = (TextView) findViewById(R.id.title1);
+        TextView subjectTitle_tv = findViewById(R.id.subject);
+        TextView description_tv = findViewById(R.id.description);
+        TextView teacher_tv = findViewById(R.id.teacher);
+        TextView credits_tv = findViewById(R.id.credits);
         Log.d("myTag", "This is the  variable value: " + intValue);
-        textView.setText("text you want to display" + intValue);
 
 
+        subjectTitle_tv.setText(titles[intValue]);
+        description_tv.setText(descriptions[intValue]);
+        teacher_tv.setText(teachers[intValue]);
+        credits_tv.setText(credits[intValue]);
     }
 }
--- app/src/main/java/com/android/courses/MainActivity.java
+++ app/src/main/java/com/android/courses/MainActivity.java
@@ -9,62 +9,34 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int[] subjects = {R.id.subject1, R.id.subject2, R.id.subject3, R.id.subject4, R.id.subject6, R.id.subject6};
+    int[] subjects = {R.id.subject1, R.id.subject2, R.id.subject3, R.id.subject4, R.id.subject5, R.id.subject6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int id : subjects) {
-            LinearLayout subject = findViewById(id);
-            subject.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    showToast(1, id);
-                    openDetailsActivity(id);
-                }
+        for (int i = 0; i < subjects.length; i++) {
+            LinearLayout subject = findViewById(subjects[i]);
+            int pos = i;
+            subject.setOnClickListener(v -> {
+                //showToast(1, i);
+                openDetailsActivity(pos);
             });
         }
-
-   /*       LinearLayout course2 = findViewById(R.id.course2);
-        course2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast(1);
-                openDetailsActivity();
-                //Intent picture_intent = new Intent(MainActivity.this,Activity2.class);
-                //startActivity(picture_intent );
-            }
-        });
-      LinearLayout course3 = findViewById(R.id.course3);
-        course3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast(1);
-                openDetailsActivity();
-            }
-        });*/
     }
 
-    private void showToast(int type, int id) {
+    private void showToast(int id) {
         Toast t;
         t = Toast.makeText(MainActivity.this,
                 "click" + id, Toast.LENGTH_SHORT);
-        if (type == 0) {
-            t = Toast.makeText(MainActivity.this,
-                    "click", Toast.LENGTH_SHORT);
-        } else if (type == 1) {
-            //PlayerX Win
-        } else {
-            ;//PlayerX Win
-        }
         t.show();
     }
 
-    public void openDetailsActivity(int intSubject) {
+    public void openDetailsActivity(int pos) {
+        //showToast(pos);
         Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
-        intent.putExtra("subjectID", intSubject);
+        intent.putExtra("subjectID", pos);
         startActivity(intent);
     }
 }
